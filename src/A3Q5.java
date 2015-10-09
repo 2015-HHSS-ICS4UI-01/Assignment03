ccdb0781e17481b7014ce1fb3bfc7e5d4cd095fe
valet8115 <valet8115@HRH-IC0027322.wrdsb.ca>
2015-10-09 14:24:14 -0400

    done

--- src/A3Q5.java
+++ src/A3Q5.java
@@ -18,19 +18,28 @@ public class A3Q5 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         A3Q5 test = new A3Q5();
+        int [] n = {60,8,32,1,45,78,34,7};//n array with values    
+//        test.insertionSort(n);//sort the numbers
+        test.insertionSort(n);
+        
+        for (int i = 0; i < n.length; i++) {// i will equal the length of the array
+            System.out.println(n[i]);//print out each number
+        }
         
     }
    public void insertionSort(int[] n){
-   for(int i = 1; i < n.length; i++)
+   for(int i = 1; i < n.length; i++)//will run the whole length of th array
    {
-      int j = i;
-      while(j>0 && n[j-1] < n[j])
+      int j = i;//j is equal to the curret i value
+      while(j>0 && n[j-1] < n[j])//while we have a number to sort j will be an array number
       {
-         int temp = n[j];
-         n[j] = n[j-1];
-         n[j-1] = temp;
-         j--;
+         int temp = n[j];//this will be a temp number with the value of what ever j is
+         n[j] = n[j-1]; //spot j will equal spot j - 1
+         n[j-1] = temp; //spot j will equal the temp number
+         j--; // subtract 1 from j 
       }
    }
+   
 }
+   
 }
