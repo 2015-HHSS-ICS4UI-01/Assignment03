11f7de28394bbebf4cf37d02849fd23e99dc5d95
janaj4926 <janaj4926@HRH-IC0027341.wrdsb.ca>
2015-10-14 11:20:57 -0400

    done

--- src/A3Q6.java
+++ src/A3Q6.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author janaj4926
  */
-public class A4Q6 {
+public class A3Q6 {
 
     /**
      * @param args the command line arguments
---
+++ src/A3Q7.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author janaj4926
+ */
+public class A3Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Pro-tip: once you're done type 'DONE'");
+        int c = 1;
+        String[] words = new String[c];
+
+        for (int i = 0; i < c; i++) {
+            System.out.println("plz enter a word:");
+            words[i] = in.nextLine();
+            if (words[i].equals("DONE")) {
+                break;
+            }
+        }
+        String temp = "";
+        for (int i = 1; i < words.length; i++) {
+            if (words[i - 1].compareTo(words[i]) > 0) {
+                temp = words[i];
+                words[i] = words[i - 1];
+                words[i - 1] = temp;
+            }
+        }
+        for (int i = 0; i < words.length; i++) {
+            System.out.print(words[i] + ", ");
+        }
+    }
+}
